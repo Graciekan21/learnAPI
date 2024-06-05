99ad4b114cc9243accb7385ed594d11c2f310bc0
Gracie Nemusesu <graciekan21@gmail.com>
2024-06-05 22:05:30 +0000

    added create function to Likeserializers to handle

---
+++ likes/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, permissions
+from LearnAPI.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    """ List all likes. Create a like if authenticated. The perform_create method associates the like with the logged in user. """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
