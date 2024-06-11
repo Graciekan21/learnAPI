2f6bdcf57a046ce3413117cf4636ab19d87a4e6f
Gracie Nemusesu <graciekan21@gmail.com>
2024-06-11 22:26:30 +0000

    resolve the remaining validation issues

--- LearnAPI/views.py
+++ LearnAPI/views.py
@@ -10,5 +10,36 @@ from .settings import (
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to my LearnAPI!"
+        "message": "Welcome to my LearnAPI!!"
     })
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    """
+    Addresses an issue with:
+dj-rest-auth contains a bug preventing users from logging out.
+We set both cookies to an empty string and include additional 
+attributes such as secure, httponly, and samesite, which were 
+mistakenly omitted by the library.
+"""
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
