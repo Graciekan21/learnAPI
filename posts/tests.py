1c386a194a5c0a5373e33867cd1e09c3764e4b54
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-01 09:55:10 +0000

    added file in the gitignore file

--- posts/tests.py
+++ posts/tests.py
@@ -22,7 +22,7 @@ class PostListViewTests(APITestCase):
         response = self.client.post('/posts/', {'title': 'a title'})
         count = Post.objects.count()
         self.assertEqual(count, 1)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
