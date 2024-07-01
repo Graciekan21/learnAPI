ea5b9326b116bdfcf6b634f75e6adefe21c7b5d7
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-01 23:17:15 +0000

    Updated the image in posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -29,10 +29,11 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True
+        upload_to=rename_file,
+        default='posts_images/default_post.jpg', blank=True
     )
     image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices, default='normal'
+        upload_to='images/', default='../media/images/default_post_pexels-steve-8603638.jpg', blank=True
     )
 
     class Meta:
