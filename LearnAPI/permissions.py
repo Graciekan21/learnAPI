1210b2a57647180ac99d456ef765bd831e3c63f4
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-08 11:09:25 +0000

    added staticfils to setting.py and Debugging

--- LearnAPI/permissions.py
+++ LearnAPI/permissions.py
@@ -2,7 +2,11 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-  def has_object_permission(self, request, view, obj):
+   """
+  Returns a boolean indicating whether the user has read access by default. 
+  If not it checks if the user is the owner of the object
+  """
+def has_object_permission(self, request, view, obj):
     if request.method in permissions.SAFE_METHODS:
       return True
     return obj.owner == request.user
