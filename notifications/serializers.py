928262b4e00d1a331f575dc0cd83cb1564b44562
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-14 22:16:58 +0000

    Added user serialization with notification

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Notification
-        fields = '__all__'
+        fields = ['id', 'user', 'username', 'message', 'is_read', 'post', 'timestamp']
