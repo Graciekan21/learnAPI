b5f8de3c93d8eba9884c5ce7b7a1611e6edd024a
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-18 04:49:41 +0000

    debugging reports

--- reports/models.py
+++ reports/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from comments.models import Comment
 class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    reported_content = models.TextField(default='1')
-    reason = models.TextField(default='1')
+    reported_content = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='comments')
+    reason = models.TextField()
+
     timestamp = models.DateTimeField(auto_now_add=True)
     is_resolved = models.BooleanField(default=False)
 
