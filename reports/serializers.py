c50ced4058e71e12f9c09f973c4494e13405498a
Gracie Nemusesu <graciekan21@gmail.com>
2024-07-18 07:43:06 +0000

    added new serialization Reports field

--- reports/serializers.py
+++ reports/serializers.py
@@ -4,7 +4,7 @@ from .models import Report
 class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
-        fields = ['reported_content', 'reason','is_resolved']
+        fields = ['id','reported_content', 'reason','is_resolved']
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
