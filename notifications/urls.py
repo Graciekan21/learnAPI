66417a6b81f6d6743df271cd763d8300af3b3126
Gracie Nemusesu <graciekan21@gmail.com>
2024-09-06 09:15:46 +0000

    added notification delete

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NotificationViewSet
+from .views import NotificationViewSet, NotificationDelete
 
 router = DefaultRouter()
 router.register(r'mynotifications', NotificationViewSet,
                 basename='mynotification')
+                
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('notifications/delete/<int:id>/', NotificationDelete.as_view()),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import DestroyAPIView
 from .models import Notification
 from .serializers import NotificationSerializer
 
@@ -16,3 +17,17 @@ class NotificationViewSet(viewsets.ModelViewSet):
             #print(f"Error fetching notifications: {e}")
             return Notification.objects.none()
 
+class NotificationDelete(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Notification.objects.all()
+    lookup_field = 'id'
+
+    def delete(self, request, id, *args, **kwargs):
+        try:
+            notification = self.queryset.get(id=id, user_id=request.user.id)
+            notification.delete()  
+            return Response({"message": "Notification deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+        except Notification.DoesNotExist:
+            return Response({"error": "Notification not found."}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
