7cc8e817cd261c07954bdd1ff9a081e08fe30d12
Gracie Nemusesu <graciekan21@gmail.com>
2024-09-06 09:59:28 +0000

    debugging notifications

--- notifications/views.py
+++ notifications/views.py
@@ -26,7 +26,7 @@ class NotificationDelete(DestroyAPIView):
         try:
             notification = self.queryset.get(id=id, user_id=request.user.id)
             notification.delete()  
-            return Response({"message": "Notification deleted successfully.id="+id+"__ user_id="+request.user.id}, status=status.HTTP_200_OK)
+            return Response({"message": "Notification deleted successfully.id="+str(id)+"__ user_id="+str(request.user.id)}, status=status.HTTP_200_OK)
         except Notification.DoesNotExist:
             return Response({"error": "Notification not found."}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
