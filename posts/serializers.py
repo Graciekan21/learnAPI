4683c618eab2455357539650895bdd6549963d66
Gracie Nemusesu <graciekan21@gmail.com>
2024-09-08 00:01:39 +0000

    debugging new field for notification

--- posts/serializers.py
+++ posts/serializers.py
@@ -57,15 +57,14 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 @receiver(post_save, sender=Post)
-def create_post_notifications(self,sender, instance, created, **kwargs):
-       myuser = self.context['request'].user.username
+def create_post_notifications(sender, instance, created, **kwargs):
        if created:
         profiles_with_notifications = Profile.objects.filter(notifications_on=True).select_related('owner')
         for profile in profiles_with_notifications:
             user = profile.owner
             Notification.objects.create(
                 user=user,
-                by=myuser,
+                by=instance.owner.username,
                 message=f'{instance.title}',
                 post_id=instance.id
             )
